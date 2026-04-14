82ad59b36a76b02c4895e6ebf5860d774ff785a7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-14 15:32:27 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,3 +17,7 @@ chmod +x /usr/bin/mkdwarfs
 
 wget https://github.com/VHSgunzo/squashfs-tools-static/releases/download/v4.7.5/mksquashfs-"$ARCH" -O /usr/bin/mksquashfs
 chmod +x /usr/bin/mksquashfs
+
+wget https://github.com/VHSgunzo/uruntime/releases/download/v0.5.7/uruntime-appimage-dwarfs-lite-"$ARCH" -O /usr/bin/uruntime-appimage-dwarfs-lite-"$ARCH"
+wget https://github.com/VHSgunzo/uruntime/releases/download/v0.5.7/uruntime-appimage-squashfs-lite-"$ARCH" -O /usr/bin/uruntime-appimage-squashfs-lite-"$ARCH"
+chmod +x /usr/bin/uruntime*
