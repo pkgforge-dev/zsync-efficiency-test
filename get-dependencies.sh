ba90be63659de227058c6f709af2fd3e558930f9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-14 15:03:00 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,8 +12,8 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano kiconthemes-mini
 
-wget https://github.com/mhx/dwarfs/releases/download/v0.15.3/dwarfs-universal-0.15.3-Linux-"$ARCH" -O /usr/bin/dwarfs
-chmod +x /usr/bin/dwarfs
+wget https://github.com/mhx/dwarfs/releases/download/v0.15.3/dwarfs-universal-0.15.3-Linux-"$ARCH" -O /usr/bin/mkdwarfs
+chmod +x /usr/bin/mkdwarfs
 
 wget https://github.com/VHSgunzo/squashfs-tools-static/releases/download/v4.7.5/mksquashfs-"$ARCH" -O /usr/bin/mksquashfs
 chmod +x /usr/bin/mksquashfs
